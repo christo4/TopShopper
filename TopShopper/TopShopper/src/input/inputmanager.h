92dadeb22cb826eaac8af71b1e3b047efc662b8d
christo4 <christopher.rodrigue@ucalgary.ca>
2019-01-30 11:04:26 -0700

    Update inputmanager.h

--- TopShopper/TopShopper/src/input/inputmanager.h
+++ TopShopper/TopShopper/src/input/inputmanager.h
@@ -1,11 +1,32 @@
-
-
-
 #ifndef INPUTMANAGER_H_
 #define INPUTMANAGER_H_
 
 
 class Broker;
+
+
+
+struct gamePad {
+	float leftAnalogX;
+	float leftAnalogY;
+	float rightAnalogX;
+	float rightAnalogY;
+	float leftTrigger;
+	float rightTrigger;
+	bool aButton;
+	bool xButton;
+	bool bButton;
+	bool yButton;
+	bool leftBump;
+	bool rightBump;
+	bool leftStick;
+	bool rightStick;
+};
+
+
+
+
+
 
 class InputManager {
 public:
