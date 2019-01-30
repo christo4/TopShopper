8fd065b5cd1ae0a9f680d2e845e347401455023c
Marcello Di Benedetto <marcello.dibenede1@ucalgary.ca>
2019-01-29 23:35:44 -0700

    Add files via upload

--- TopShopper/TopShopper/src/core/main.cpp
+++ TopShopper/TopShopper/src/core/main.cpp
@@ -1,22 +1,62 @@
-
-
-
 #include "broker.h"
+#include <iostream>
 
 int main() {
-	// ~~~~~load assets (init necessary data structures)???
-
-	// fields...
+	//load assets here
 	double prevTime = 0.0;
 	Broker* broker = Broker::getInstance();
-
-	// work...
-	broker->initAll(); // init all subsystems
-
+	broker->initAll();
 	GLFWwindow* window = broker->get_RenderingManager_Window();
 
+
+
 	// call main loop
-	while (!glfwWindowShouldClose(window)) { // ~~~~~~~~~ also probably add || QUIT menu option was pressed.
+	while (!glfwWindowShouldClose(window)) {
+
+		
+		int gamePadPresent = glfwJoystickPresent(GLFW_JOYSTICK_1);
+		//std::cout << "Gamepad Status: " << gamePadPresent << std::endl;
+
+		int numAxes;
+		int numButtons;
+		const float * axesArray = NULL;
+		const unsigned char * buttonArray = NULL;
+
+
+
+		if (gamePadPresent) {
+
+			axesArray = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &numAxes);
+			std::cout << std::endl;
+			std::cout << std::endl;
+
+			
+			std::cout << "Left Stick X: " << axesArray[0] << std::endl;
+			std::cout << "Left Stick Y: " << axesArray[1] << std::endl;
+			std::cout << "Right Stick X: " << axesArray[2] << std::endl;
+			std::cout << "Right Stick Y: " << axesArray[3] << std::endl;
+			std::cout << "Left Trigger: " << axesArray[4] << std::endl;
+			std::cout << "Right Trigger: " << axesArray[5] << std::endl;
+
+			
+
+
+			buttonArray = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &numButtons);
+
+			//button array: A B X Y LB RB LEFTSTICK RIGHTSTICK UP RIGHT LEFT DOWN
+
+			/*
+			for (int i = 0; i < numButtons; i++) {
+
+				if (buttonArray[i] == GLFW_PRESS) {
+					std::cout << i << " " << "press" << std::endl;
+				}
+			}
+
+			*/
+		}
+
+
 		// 1. get deltaTime (time last frame took to execute)
 		// 2. get device input
 		// 3. simulate (update game state)
@@ -26,8 +66,6 @@ int main() {
 		// new way:
 		// 1. get deltaTime
 		// 2. call Broker::getInstance()->updateAllMilliseconds(deltaTime);
-
-
 		// ~~~~~NOTE: I think that the first deltaTime could screw up some stuff
 		double currentTime = glfwGetTime(); // get new
 		double deltaTime = currentTime - prevTime; // diff
