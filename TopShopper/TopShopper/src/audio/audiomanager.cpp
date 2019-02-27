26fa3f00fd6d2deb3aa51a1a241d68600e99a719
Aaron Hornby (Laptop) <aaron.hornby@outlook.com>
2019-02-26 19:27:45 -0700

    merged in audioNew branch

--- TopShopper/TopShopper/src/audio/audiomanager.cpp
+++ TopShopper/TopShopper/src/audio/audiomanager.cpp
@@ -1,6 +1,14 @@
 #include "audiomanager.h"
 #include "core/broker.h"
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+AudioManager::SoundEffect* testSoundEffect = new AudioManager::SoundEffect();
 
 
 AudioManager::AudioManager(Broker *broker) 
@@ -10,13 +18,125 @@ AudioManager::AudioManager(Broker *broker)
 }
 
 AudioManager::~AudioManager() {
+	// quit SDL_mixer
+	Mix_Quit();
+}
 
+AudioManager::SoundEffect* AudioManager::getSoundEffect(SoundEffectTypes sound) {
+	switch (sound) {
+	case SoundEffectTypes::TEST_SOUND:
+		return testSoundEffect;
+	default:
+		return nullptr;
+	}
+}
+
+
+Mix_Music* AudioManager::loadMusic(string filename) {
+	//string fullPath = SDL_GetBasePath();
+	//string fullPath = filename;
+
+	//fullPath.append(filename);
+
+	if (mMusic[filename] == nullptr) {
+		mMusic[filename] = Mix_LoadMUS(filename.c_str());
+		if (mMusic[filename] == NULL)
+			cout << "Music Loading Error" << Mix_GetError() << endl;
+	}
+	return mMusic[filename];
+}
+
+Mix_Chunk* AudioManager::loadSFX(string filename) {
+	//string fullPath = SDL_GetBasePath();
+
+	//fullPath.append(filename);
+	//printf("%s\n", fullPath.c_str());
+
+	if (mSFX[filename] == nullptr) {
+		mSFX[filename] = Mix_LoadWAV(filename.c_str());
+		if (mSFX[filename] == NULL) {
+			cout << "SFX loading error"<< Mix_GetError()  << endl;
+		}
+	}
+	return mSFX[filename];
+}
+
+// play myMusic
+void AudioManager::playMusic(Music *myMusic) {
+	Mix_VolumeMusic(myMusic->volume);
+	//Mix_PlayMusic(myMusic->music, myMusic->loop);
+	if (Mix_PlayMusic(myMusic->music, myMusic->loop) == -1) {
+		printf("Mix_PlayMusic: %s\n", Mix_GetError());
+	}
+}
+
+// pause music
+void AudioManager::pauseMusic() {
+	if (Mix_PlayingMusic() != 0)
+		Mix_PauseMusic();
+}
+
+// resume the paused music
+void AudioManager::resumeMusic() {
+	if (Mix_PausedMusic() != 0)
+		Mix_ResumeMusic();
+}
+
+void AudioManager::playSFX(SoundEffect *mySfx) {
+	Mix_SetPosition(mySfx->channel, mySfx->angle, mySfx->distance);
+	Mix_PlayChannel(mySfx->channel, mySfx->sfx, mySfx->loop);
+	//Mix_PlayChannelTimed(mySfx->channel, mySfx->sfx, mySfx->loop, mySfx->time);
+	//SDL_Delay(mySfx->time);
 }
 
 void AudioManager::init() {
 
+	// init SDL
+	SDL_Init(SDL_INIT_AUDIO);
+	// init SDL_mixer
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096) < 0) {
+		cout << "mixer initialization error" << endl;
+	}
+
+	// allocate sound effect channel  
+	Mix_AllocateChannels(16);
+
+	// load sound effect from file
+	for (int i = 0; i < filenames.size(); i++) {
+		SoundEffect sfx;
+		sfx.filename = filenames[i];
+		sfx.sfx = loadSFX(sfx.filename);
+		sfx.channel = i; // allocate the channel for sound effect
+		soundEffects.push_back(sfx);
+	}
+
+	testSoundEffect->filename = "../TopShopper/resources/sfx/test2.wav";
+	testSoundEffect->sfx = loadSFX(testSoundEffect->filename);
+	testSoundEffect->channel = 0;
+
+
+
+	
+
+	// load music from file
+	bgm.music = loadMusic(bgm.filename);
+	cout << "load bgm file" << endl;
+	playMusic(&bgm);
+
+
+	
+
 }
 
 void AudioManager::updateSeconds(double variableDeltaTime) {
+	// play all the sound effects insidethe soundeffect vector
+	//for (int i = 0; i < soundEffects.size(); i++) {
+	//	playSFX(&soundEffects[i]);
+	//}
+	//resumeMusic();
+	//playSFX(testSoundEffect);
+	
+	
 
 }
+
--- TopShopper/TopShopper/src/audio/audiomanager.h
+++ TopShopper/TopShopper/src/audio/audiomanager.h
@@ -1,34 +1,65 @@
 #ifndef AUDIOMANAGER_H_
 #define AUDIOMANAGER_H_
 
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <map>
+#include <vector>
+#include <string>
+#undef main
+
 class Broker;
+
+enum SoundEffectTypes {
+	TEST_SOUND
+};
 
 class AudioManager {
 public:
+	struct SoundEffect {
+		std::string filename;
+		Mix_Chunk* sfx;
+		//map<string, Mix_Chunk*> mySFX; // <full path, acutal chunk>
+		Uint8 distance = 200;  // 0 at your loaction
+		Sint16 angle = 0;  // 0 at front, 90 at right, 180 at back, 270 at left
+		int time = 1500; // time to play in millisecond also is the time delay
+		int channel; // the channel that play the sound effect (channel >= 0)
+		int loop = 0; // -1 for infinite loop, 0 for play once, more than 2 for number of rounds
+	};
+	
+	struct Music {
+		std::string filename = "../TopShopper/resources/sfx/SuperMarioBros.mp3";
+		Mix_Music* music;
+		int volume = MIX_MAX_VOLUME / 32;  // only work for mod, mp3 and ogg files
+		int loop = -1; // number of times to play, -1 forever, bigger than 0(number of times)
+	};
+
 	AudioManager(Broker *broker);
 	virtual ~AudioManager();
 	void init();
 	void updateSeconds(double variableDeltaTime);
+
+	SoundEffect* getSoundEffect(SoundEffectTypes sound);
+	void playMusic(Music * myMusic);
+	void pauseMusic();
+	void resumeMusic();
+
+	void playSFX(SoundEffect *mySfx);
+
+	Mix_Music* loadMusic(std::string filename);
+	//Mix_Chunk* loadSFX(SoundEffect *mySfx);
+	Mix_Chunk* loadSFX(std::string filename);
+	//SoundEffect testSoundEffect;
 private:
 	Broker *_broker = nullptr;
+
+	std::map<std::string, Mix_Music*> mMusic;
+	std::map<std::string, Mix_Chunk*> mSFX;
+
+	Music bgm;
+	std::vector<std::string> filenames = { "../TopShopper/resources/sfx/test2.wav" };
+	std::vector<SoundEffect> soundEffects;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
