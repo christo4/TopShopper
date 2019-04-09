ca4e8e4f81a0b328eba96c7a74bdb3b99421ac01
Aaron Hornby (Desktop) <aaron.hornby@outlook.com>
2019-04-08 21:28:52 -0600

    order fix

--- TopShopper/TopShopper/src/rendering/renderingmanager.cpp
+++ TopShopper/TopShopper/src/rendering/renderingmanager.cpp
@@ -309,9 +309,7 @@ glm::mat4 RenderingManager::computeCameraPosition(int playerID, glm::vec3 &camer
 	// get the average vehicle rotation in last 10 frames (incl. this one) 
 	// have to use vectors since thetas have edge case problems (e.g. going from pi to -pi)
 	PxVec3 vehicleRotationVecSum(0.0f, 0.0f, 0.0f);
-	std::deque<float> gVehicleThetas = gVehicleThetasMap[playerID];
-	std::deque<float> gRightStickXValues = gRightStickXValuesMap[playerID];
-
+	const std::deque<float> &gVehicleThetas = gVehicleThetasMap[playerID];
 
 	for (float t : gVehicleThetas) {
 		// assume a radius of 1 for this calculation (scale later by our intended radius)
@@ -330,6 +328,8 @@ glm::mat4 RenderingManager::computeCameraPosition(int playerID, glm::vec3 &camer
 		gRightStickXValuesMap[playerID].pop_front();
 		gRightStickXValuesMap[playerID].push_back(newRightStickXValue);
 		float avgRightStickXValue = 0.0f;
+		const std::deque<float> &gRightStickXValues = gRightStickXValuesMap[playerID];
+
 		for (float f : gRightStickXValues) {
 			avgRightStickXValue += f;
 		}
