9c5d64a611bdb2b8b6596fd215b9d9ab25f0837e
Marcello Di Benedetto <marcello.dibenede1@ucalgary.ca>
2019-04-12 00:22:24 -0600

    Spotlight Rendering

--- TopShopper/TopShopper/src/rendering/Geometry.cpp
+++ TopShopper/TopShopper/src/rendering/Geometry.cpp
@@ -7,7 +7,7 @@
 
 #include "Geometry.h"
 
-Geometry::Geometry() : vao(0), vertexBuffer(0), normalBuffer(0), uvBuffer(0), colorBuffer(0), gradientShader(false), cullBackFace(false), isTransparent(false), hasShadow(true), pointer(false), player(-1){
+Geometry::Geometry() : vao(0), vertexBuffer(0), normalBuffer(0), uvBuffer(0), colorBuffer(0), gradientShader(false), cullBackFace(false), isTransparent(false), hasShadow(true), pointer(false), player(-1), EntityType(NONE), transDegree(1.0f){
 	//vectors are initially empty
 	//Pointers are initially null
 	//Call RenderingEngine::assignBuffers and RenderingEngine::setBufferData to fully initialize the geometry
